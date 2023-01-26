738fca1f29644a4f2e5263201accba640a70dd6a
David LEROUX <dleroux63160@gmail.com>
2023-01-26 17:41:48 +0100

    make edit form and install json server API REST make service and db file for listing pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,29 @@ import PokemonList from './pages/pokemon-list';
 import PokemonDetail from './pages/pokemon-detail';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PageNotFound from './pages/page-not-found';
+import PokemonEdit from './pages/pokemon-edit';
 
 const App: FunctionComponent = () => {
 
     return (
-     <Router>
-        <div>
-            {/*la barre de navigation commune a toutes nos pages */}
-            <nav>
-                <div className="nav-wrapper teal">
-                    <Link to="/" className="brand-logo center">Pokédex</Link>
-                </div>
-            </nav>
-             {/* système de gestion des routes de notre application */}
-             <Switch>
-                <Route exact path="/" component={PokemonList} />
-                <Route exact path="/pokemons" component={PokemonList} />
-                <Route exact path="/pokemons/:id" component={PokemonDetail} />
-                <Route component={PageNotFound} />
-             </Switch>
-        </div>
-     </Router>
+        <Router>
+            <div>
+                {/*la barre de navigation commune a toutes nos pages */}
+                <nav>
+                    <div className="nav-wrapper teal">
+                        <Link to="/" className="brand-logo center">Pokédex</Link>
+                    </div>
+                </nav>
+                {/* système de gestion des routes de notre application */}
+                <Switch>
+                    <Route exact path="/" component={PokemonList} />
+                    <Route exact path="/pokemons" component={PokemonList} />
+                    <Route exact path="/pokemons/edit/:id" component={PokemonEdit} />
+                    <Route exact path="/pokemons/:id" component={PokemonDetail} />
+                    <Route component={PageNotFound} />
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
